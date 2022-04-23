5ec4ed8f59d40b7fb620e9339c43d9862243495f
Victor Ortega A <v.ortegaaz@gmail.com>
2022-04-23 01:50:59 -0400

    PHP Mailer, usa ssgalaxyii22 para pruebas

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0f062b05ab3b662a2ec9262344a492982bb56ab5',
+        'name' => 'creativetimofficial/material-dashboard',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'creativetimofficial/material-dashboard' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0f062b05ab3b662a2ec9262344a492982bb56ab5',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.0',
+            'version' => '6.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'e43bac82edc26ca04b36143a48bde1c051cfd5b1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
